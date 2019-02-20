d7aa0e1fa71182ce1907d612a83a677b05c18903
Archerylover98 <47616441+Archerylover98@users.noreply.github.com>
2019-02-19 20:11:28 -0600

    Add files via upload

---
+++ N.McKee - IN200 - Chapter 3 Assignment.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+  const int Inventory_Max = 10;
+  string inventory[Inventory_Max];
+  int Gear = 0;
+  inventory[Gear++] = "Sword";
+  inventory[Gear++] = "Armor";
+  inventory[Gear++] = "Shield";
+  cout <<"Your Starting Gear:\n" << endl;
+  for (int i = 0; i < Gear; ++i)
+  {
+    cout << inventory[i] << endl;
+  }
+  cout << endl <<"Congratulations on acquiring starter gear for your adventure, Hero.\n" << endl;
+  cout <<"However, you will acquire new gear and items during your adventures.\n" << endl;
+  cout <<"This acquisition is in aid of you succeeding against the demon lord." << endl << endl;
+  cout <<"You shall find these new items due to our need for the demon to die." << endl << endl;
+  cout <<"Good luck on your travels Hero, we will pray for your success." << endl << endl;
+  cout <<"You have defeated the first boss of 5 on your journey, Hero." << endl;
+  cout <<"Your reward is a new weapon, which you shall name." <<endl <<endl;
+  int Weapon;
+  cin >> Weapon;
+  cout <<endl << "Your new Weapon's name is: ";
+  cout << Weapon <<endl <<endl;
+  cout <<"You have defeated your second boss. Congratulations." <<endl;
+  cout <<"Your reward is an infinite-use healing potion. Name your new artifact." << endl;
+
+  int Potion;
+  cin >> Potion;
+  cout <<"Your new potion is named: " <<endl;
+  cout << Potion <<endl <<endl;
+
+  cout <<"The third boss has fallen before you, well-done." << endl;
+  cout <<"Your reward is an enchanted Necklace of protection. Name it." <<endl;
+  int Necklace;
+  cin >> Necklace;
+  cout <<"Your new enchanted necklace is named: ";
+  cout << Necklace <<endl;
+  return 0;
+}
